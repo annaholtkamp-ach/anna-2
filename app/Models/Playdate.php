3ae186cf25a862c6089135eb67b0e7723bf1e1f8
annaholtkamp-ach <anna.holtkamp@code.berlin>
2025-09-05 17:48:55 +0200

    playdate model is doing fine

---
+++ app/Models/Playdate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Playdate extends Model
+{
+    /** @use HasFactory<\Database\Factories\PlaydateFactory> */
+    use HasFactory;
+}
