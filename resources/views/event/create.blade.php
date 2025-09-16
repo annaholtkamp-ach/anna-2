4caf1aa3557d4c2244104ff7261e039e83a897c7
annaholtkamp-ach <anna.holtkamp@code.berlin>
2025-09-16 21:45:57 +0200

    Title input form

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -4,7 +4,13 @@
 </head>
 <div>
     form will be added
-    
+    <form action="/event" method="post">
+    @csrf
+        <div>
+            <label for="title"> Title</label><br/>
+            <input type="text" name="title" class="bg-gray-200 p-2">
+        </div>
+        
 </div>
 
 
